3c19506cef024fcf0c0271a0e36975e0a8530b22
Mohamed Sayed <ms019070@gmail.com>
2020-06-16 21:44:49 +0300

    dont remove the main.js from the index.html

--- temp/main.js
+++
@@ -1,7 +0,0 @@
-let myInput = document.getElementById("input");
-let myDiv = document.getElementById("div");
-
-myInput.onkeypress = function(){
-    alert("WELCOME TO THE ULTIMATE CONVERTER");
-    myDiv.innerHTML = myInput.value * 3.75;
-}
